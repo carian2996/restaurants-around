e4278817aeab4d8960fa2f2ab36ecb864e16bba7
Ian Castillo Rosales <carian2996@gmail.com>
2020-05-22 19:12:10 -0500

    Scraper ready to work over UE

--- scraper/ue_scraper.py
+++ scraper/ue_scraper.py
@@ -40,26 +40,26 @@ if __name__ == '__main__':
         # Initiate driver in URL
         driver.get(URL)
         
-        time.sleep(5) # from time to tim I'll wait a cuple of seconds
+        time.sleep(5) # from time to tim I'll wait a cuple of secon3s
         print(str(datetime.now()), '- Input address...')
         # Looking for input combobox in main page
         location_input = driver.find_element_by_id('location-typeahead-home-input')
         location_input.send_keys(ADDRESS)
 
-        time.sleep(5)
+        time.sleep(3)
         print(str(datetime.now()), '- Searching restaurants...')
         # Sending keys to establish the ADDRESS
         search_button = driver.find_element_by_xpath('//button[text()="Buscar comida"]')
         search_button.click()
 
-        time.sleep(5)
+        time.sleep(3)
         # Once our ADDRESS was inputed, we need all restaurants around there
 
         # TODO: Apparently /near-me is generic and independent from the city or address
         # I'm good with this, but vulnerable...
         driver.get(URL+'/near-me')
 
-        time.sleep(5)
+        time.sleep(3)
         print(str(datetime.now()), '- Retriving food categories...')
         # Uber Eats groups restaurants near you in food categories
         categories = driver.find_elements_by_xpath('//a[@href]')
@@ -75,48 +75,66 @@ if __name__ == '__main__':
         categories_hrefs = [c.get_attribute("href") for c in categories if re.search(r'.*\/near-me\/.*([a-z])$', c.get_attribute("href"))]
         
         data = {}
-        for h in categories_hrefs:
-            time.sleep(10)
-            cate = h.replace(URL + '/near-me/', '')
+        for c in categories_hrefs:
+            cate = c.replace(URL + '/near-me/', '')
+            time.sleep(5)
 
             data[cate] = {}
             
-            time.sleep(5)
-            data[cate]['web_href'] = h.replace('https://www.ubereats.com', '')
+            time.sleep(3)
+            data[cate]['web_href'] = c.replace('https://www.ubereats.com', '')
 
-            time.sleep(5)
+            time.sleep(3)
             # FIXED: Some elements can be unavailable at moments, adding an try-expection
             try:
                 # I iterate for all categories available and go to another page
-                driver.find_element_by_xpath("//a[@href='" + data[cate]['web_href'] + "']").click()
+                driver.get(c)
                 
             except Exception as e:
-                print('\n'+'Element', data[cate]['web_href'], 'is not clickable at point')
-                continue
+                raise print(e)
 
             data[cate]['restaurants'] = {}
             
-            time.sleep(5)
+            time.sleep(3)
             print('\n' + str(datetime.now()), '- Looking restaurants for:', cate, '\n')
             # Same process... lots of links, I only need links ot get restaurant's data
             restaurants = driver.find_elements_by_xpath("//a[@href]")
             restaurants_hrefs = [c.get_attribute("href") for c in restaurants if re.search(r'\/food-delivery\/', c.get_attribute("href"))]
             
             for r in restaurants_hrefs:
-                # TODO: Only works for Mexico City
-                id = r.replace(URL + '/mexico-city/food-delivery/', '').split('/')[0]
+                time.sleep(1)
                 try:
-                    name = driver.find_element_by_xpath("//a[@href='" + r.replace(URL[:-3], '') + "']").text
-                    data[cate]['restaurants'][id] = {}
-                    data[cate]['restaurants'][id]['name'] = name
-                    print(name)
-                except Exception as e:
-                    continue
-            
-            driver.back()
+                    # Visit restaurant page to get detailed data
+                    driver.get(r)
+                    
+                    try:
+                        script = driver.find_element_by_xpath("//script[@type='application/ld+json']")
+                    except Exception as e:
+                        continue
+                    details = json.loads(script.get_attribute('innerHTML'))
 
+                    # TODO: Only works for Mexico City. Removes random identifier at the end
+                    resto_id = details['@id'].replace(URL + '/mexico-city/food-delivery/', '').split('/')[0] 
+                    
+                    data[cate]['restaurants'][resto_id] = {}
+                    
+                    print(details['name'])
+
+                    if 'name' in details.keys(): data[cate]['restaurants'][resto_id]['name'] = details['name']
+                    if 'servesCuisine' in details.keys(): data[cate]['restaurants'][resto_id]['servesCuisine'] = details['servesCuisine']
+                    if 'geo' in details.keys(): data[cate]['restaurants'][resto_id]['geo'] = [details['geo']['latitude'], details['geo']['longitude']]
+                    if 'priceRange' in details.keys(): data[cate]['restaurants'][resto_id]['priceRange'] = details['priceRange']
+                    if 'address' in details.keys(): data[cate]['restaurants'][resto_id]['address'] = details['address']
+                    if 'telephone' in details.keys(): data[cate]['restaurants'][resto_id]['telephone'] = details['telephone']
+                    if 'aggregateRating' in details.keys(): data[cate]['restaurants'][resto_id]['aggregateRating'] = details['aggregateRating']
+                    if 'openingHoursSpecification' in details.keys(): data[cate]['restaurants'][resto_id]['openingHoursSpecification'] = details['openingHoursSpecification']
+                
+                except Exception as e:
+                    raise print(e)
+        
         print('\n')
         with open(OUTPUT, 'w', encoding='utf8') as f: 
             json.dump(data, f, ensure_ascii=False)
 
+        driver.quit()
         print(str(datetime.now()), '- Done!')
