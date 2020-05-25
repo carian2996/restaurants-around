34b4b0afd17a6685eac41abb0edbba2fe18bbb8c
Ian Castillo Rosales <carian2996@gmail.com>
2020-05-25 17:23:26 -0500

    Adding address to parser

--- scraper/parser/parser.py
+++ scraper/parser/parser.py
@@ -7,7 +7,7 @@ def parse(json, platform):
         data = json
         df = pd.DataFrame(list(data.keys()), columns=['id'])
 
-        ids, resto_name, lat, lng, cuisine = [[] for _ in range(5)]
+        ids, resto_name, lat, lng, cuisine, address = [[] for _ in range(6)]
 
         for r in df.id:
             if len(data[r]) == 0: continue
@@ -17,7 +17,23 @@ def parse(json, platform):
             lat.append(data[r]['geo'][0])
             lng.append(data[r]['geo'][1])
 
-        features = pd.DataFrame({'id': ids, 'name': resto_name, 'lat': lat, 'lng': lng, 'cuisine': cuisine})
+            try:
+                if isinstance(data[r]['address'], str):
+                    address.append(data[r]['address'])
+                elif isinstance(data[r]['address'], dict):
+                    address.append(data[r]['address']['streetAddress'])
+            except KeyError:
+                address.append('')
+
+        features = pd.DataFrame({
+            'id': ids
+            , 'name': resto_name
+            , 'lat': lat
+            , 'lng': lng
+            , 'cuisine': cuisine
+            , 'address': address
+            })
+        
         df = df.merge(features)
         df['platform'] = platform
 
